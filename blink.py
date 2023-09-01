49cb702df22c8c7acb58f6f885b707ec4c372a5c
Shinichiro Oba <bricklife@gmail.com>
2023-09-01 22:07:13 +0900

    Update blink.py

--- blink.py
+++ blink.py
@@ -3,6 +3,7 @@ import board
 import digitalio
 
 led = digitalio.DigitalInOut(board.LED)
+#led = digitalio.DigitalInOut(board.LED_INVERTED) # Seeed Studio XIAO SAMD21
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
